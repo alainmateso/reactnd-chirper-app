8c27be0bffafb972729e46fc04f76e1e4e35082f
codeAl <codeal@junior.com>
2020-06-04 16:07:57 +0200

    add tweet component and loadingBar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Tweet from './Tweet'
 
 class Dashboard extends Component {
   render() {
@@ -10,7 +11,9 @@ class Dashboard extends Component {
         <ul>
           {this.props.tweetIds.map((id) => (
             <li key={id}>
-              <div>TWEET ID: {id}</div>
+              <div>
+                <Tweet id={id} />
+              </div>
             </li>
           ))}
         </ul>
